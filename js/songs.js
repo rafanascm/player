4872d0de01919dd508bb57c1512953a9a53afc93
Rafaela Nascimento <93143251+rafanascm@users.noreply.github.com>
2024-03-04 18:30:34 -0300

    commit inicial

---
+++ js/songs.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    src: "./src/Kanye West ft Taylor Swift.mp3",
+    name: "Famous x Shake It Offkkkkk",
+  },
+
+  {
+    src: "./src/New Slaves.mp3",
+    name: "New Slaves",
+  },
+  {
+    src: "./src/Black Skinhead.mp3",
+    name: "Black Skinhead",
+  },
+  {
+    src: "./src/power.mp3",
+    name: "Power",
+  },
+  {
+    src: "./src/Runaway.mp3",
+    name: "Runaway",
+  },
+];
